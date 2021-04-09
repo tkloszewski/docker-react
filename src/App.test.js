e24788739a787aa4275698939894dc9fd119692e
TKLOSZEWSKI <Tomasz.Kloszewski@IGT.com>
2021-04-09 14:40:03 +0200

    - Fixed tests(4)

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn reactw/i);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
